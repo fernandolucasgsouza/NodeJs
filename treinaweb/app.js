839606768e7ed40299bb0311ff21a2fd44c4f0e4
fernandolucasgsouza <fernandolucass.2012@gmail.com>
2020-04-19 14:45:22 -0300

    realizando testes de module

--- treinaweb/app.js
+++ treinaweb/app.js
@@ -1,6 +1,6 @@
+import { POINT_CONVERSION_COMPRESSED } from 'constants';
 
 var GetModChachorro = require('./cachorro');
 
 console.log(GetModChachorro.nome)
-GetModChachorro.latir()
-
+GetModChachorro.latir();

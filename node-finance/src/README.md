531ac5d019056501e4c4c7467996f9136493525c
fernandolucasgsouza <fernandolucass.2012@gmail.com>
2022-01-26 20:10:24 -0300

    add middlware

--- node-finance/src/README.md
+++ node-finance/src/README.md
@@ -1,7 +1,7 @@
 ### Requisitos
 
 - [x] Deve ser possível criar uma conta
-- [] Deve ser possível buscar o extato bancário do cliente
+- [x] Deve ser possível buscar o extato bancário do cliente
 - [] Deve ser possível relizar um deposito
 - [] Deve ser possível realizar um saque
 - [] Deve ser possível buscar o extrato bancário do cliente por data
@@ -12,7 +12,7 @@
 ### Regras de negócio
 
 - [x] Não Deve ser possível cadastrar uma conta com CPF já existente
-- [] Não Deve ser possível fazer depósito em uma conta não existente
+- [x] Não Deve ser possível fazer depósito em uma conta não existente
 - [] Não Deve ser possível buscar extrato de uma conta não existente
 - [] Não Deve ser possível fazer um saque em uma conta não existente
 - [] Não Deve ser possível excluir uma conta não existente
--- node-finance/src/index.js
+++ node-finance/src/index.js
@@ -6,6 +6,19 @@ app.use(express.json());
 
 const customers = [];
 
+function verifyExistAccountCPF(request, response, next) {
+  // Middlware
+  const { cpf } = request.headers;
+  const customer = customers.find((customer) => (customer.cpf = cpf));
+
+  if (!customer)
+    return response.status(404).json({ error: "customer not found" });
+
+  request.customer = customer;
+
+  return next();
+}
+
 app.post("/account", (request, response) => {
   const { cpf, name } = request.body;
 
@@ -25,13 +38,26 @@ app.post("/account", (request, response) => {
   return response.status(201).send(customers);
 });
 
-app.get("/statement", (request, response) => {
-  const { cpf } = request.headers;
-  const customer = customers.find((customer) => (customer.cpf = cpf));
-
-  if (!customer) return response.status(404).json({ error: "customer not found" });
+app.get("/statement", verifyExistAccountCPF, (request, response) => {
+  const { customer } = request;
 
   return response.status(200).json(customer.statement);
+});
+
+app.post("/deposit", verifyExistAccountCPF, (request, response) => {
+  const { description, amount } = request.body;
+  const { customer } = request;
+
+  const statemantOpetation = {
+    description,
+    amount,
+    createdAt: new Date(),
+    type: "credit",
+  };
+
+  customer.statement.push(statemantOpetation);
+
+  return response.status(201).send(customer.statement);
 });
 
 app.put("/", (request, response) => {});
